0cb80e7e16e9d39605085f4d6fb6bb2e40a1fcee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-09-29 03:10:48 +0000

    Deploy ridiazcampos/ridiazcampos.github.io to github.com/ridiazcampos/ridiazcampos.github.io.git:gh-pages

---
+++ _next/static/b1FoZy_wTy72TPArC0x_S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-44290f7e5621719f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
