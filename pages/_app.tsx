497c274a043989de8c1a0cd0ff0b513e0d684849
Raúl Ignacio Diaz Campos <ridiaz2@uc.cl>
2022-09-29 00:08:02 -0300

    feat: add sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Header from "../components/Layout/header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex w-full h-full">
+      <div className="w-full">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
